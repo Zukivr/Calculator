71969a040da060924e0884fda43f81ac9812836a
Zuki <91832837+Zukivr@users.noreply.github.com>
2021-12-21 18:06:52 +0100

    Code
    Added exceptionhandling, no more crashing from wrong input

--- Program.cs
+++ Program.cs
@@ -8,74 +8,114 @@ namespace ConsoleApp1
 		static void Main(string[] args)
 		{
 			// My first basic calculator program by Zuki
-			// Known issues: crashing when trying to handle exception
-			// Future updates: Add exception handling through tryParse or tryCatch
 			MyCalculator();
 		}
 
 		public static void MyCalculator()
 		{
-            int menu;
-            float val1;
-            float val2;
-            float result;
-            string tryAgain;
+			int menu;
+			float val1;
+			float val2;
+			float result;
+			string tryAgain;
+			float parsed;
 
-            do
+			do
 			{
 				Console.WriteLine("Welcome to the calculator. Please choose an operation to continue, by entering the corresponding number.");
 				Console.WriteLine("1: Addition");
 				Console.WriteLine("2: Subtraction");
 				Console.WriteLine("3: Multiplication");
 				Console.WriteLine("4: Division");
-				menu = Convert.ToInt32(Console.ReadLine());
-				Console.Clear();
+				bool menuCheck = int.TryParse(Console.ReadLine(), out menu);
+				if (menuCheck == true)
+				{ 
+					Console.Clear();
 
-				switch (menu)
+					switch (menu)
+					{
+						case 1:
+							Console.WriteLine("Choose two numbers to add up.");
+							bool success = float.TryParse(Console.ReadLine(), out parsed);
+							val1 = parsed;
+							bool success2 = float.TryParse(Console.ReadLine(), out parsed);
+							val2 = parsed;
+							if (success && success2 == true)
+							{
+								result = val1 + val2;
+								Console.WriteLine("The result is: " + result);
+							}
+							else
+							{
+								Console.WriteLine("The input was incorrect, please try again.");
+							}
+							break;
+
+						case 2:
+							Console.WriteLine("Choose two numbers to subtract.");
+							success = float.TryParse(Console.ReadLine(), out parsed);
+							val1 = parsed;
+							success2 = float.TryParse(Console.ReadLine(), out parsed);
+							val2 = parsed;
+							if (success && success2 == true)
+							{
+								result = val1 - val2;
+								Console.WriteLine("The result is: " + result);
+							}
+							else
+							{
+								Console.WriteLine("The input was incorrect, please try again.");
+							}
+							break;
+
+						case 3:
+							Console.WriteLine("Choose two numbers to multiply.");
+							success = float.TryParse(Console.ReadLine(), out parsed);
+							val1 = parsed;
+							success2 = float.TryParse(Console.ReadLine(), out parsed);
+							val2 = parsed;
+							if (success && success2 == true)
+							{
+								result = val1 * val2;
+								Console.WriteLine("The result is: " + result);
+							}
+							else
+							{
+								Console.WriteLine("The input was incorrect, please try again.");
+							}
+							break;
+
+						case 4:
+							Console.WriteLine("Choose two numbers divide.");
+							success = float.TryParse(Console.ReadLine(), out parsed);
+							val1 = parsed;
+							success2 = float.TryParse(Console.ReadLine(), out parsed);
+							val2 = parsed;
+							if (success && success2 == true)
+							{
+								result = val1 / val2;
+								Console.WriteLine("The result is: " + result);
+							}
+							else
+							{
+								Console.WriteLine("The input was incorrect, please try again.");
+							}
+							break;
+
+						default:
+							Console.WriteLine("An error occured! Please try again.");
+							break;
+					}
+				}
+				if (menuCheck == false)
 				{
-					case 1:	
-						Console.WriteLine("Choose two numbers to add up.");
-						val1 = float.Parse(Console.ReadLine());	
-						val2 = float.Parse(Console.ReadLine()); 
-						result = val1 + val2;
-						Console.WriteLine("The result is: " + result);
-						break;
-
-					case 2:
-						Console.WriteLine("Choose two numbers to subtract.");
-						val1 = float.Parse(Console.ReadLine());
-						val2 = float.Parse(Console.ReadLine());
-						result = val1 - val2;
-						Console.WriteLine("The result is: " + result);
-						break;
-
-					case 3:
-						Console.WriteLine("Choose two numbers to multiply.");
-						val1 = float.Parse(Console.ReadLine());
-						val2 = float.Parse(Console.ReadLine());
-						result = val1 * val2;
-						Console.WriteLine("The result is: " + result);
-						break;
-
-					case 4:
-						Console.WriteLine("Choose two numbers divide.");
-						val1 = float.Parse(Console.ReadLine());
-						val2 = float.Parse(Console.ReadLine());
-						result = val1 / val2;
-						Console.WriteLine("The result is: " + result);
-						break;
-
-					default:
-						Console.WriteLine("An error occured! Please try again.");
-						break;
+					Console.WriteLine("Your input was wrong.");
 				}
 				Console.WriteLine("Do you want to try again? (yes/no)");
 				tryAgain = Console.ReadLine();
 				Console.Clear();
-			} while (tryAgain == "yes");
+			} while (tryAgain == "yes") ;	
 		}
 	}
-
-
 }
 
